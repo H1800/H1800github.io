5acb554efc5bc4b96fd2f9c2fefeb50ce830a4b8
H1800 <3235612820@qq.com>
2026-08-10 10:49:39 +0800

    Delete qingmiao/write_info.py

--- qingmiao/write_info.py
+++
@@ -1,6 +0,0 @@
-﻿import base64
-# Base64-encoded original info.html content
-data = ""
-with open("D:/codex/qingmiao/info.html", "w", encoding="utf-8") as f:
-    f.write(base64.b64decode(data).decode("utf-8"))
-print("Done")
